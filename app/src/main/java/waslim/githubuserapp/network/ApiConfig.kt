d1b5d4f8b1425a39e66315a64619476c4a4d44ad
Waslim <waslim.liem19240@student.unsika.ac.id>
2022-09-24 13:14:22 +0700

    first commit

--- app/src/main/java/waslim/githubuserapp/network/ApiConfig.kt
+++ app/src/main/java/waslim/githubuserapp/network/ApiConfig.kt
@@ -6,21 +6,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
-//    companion object{
-//        fun getApiService(): ApiServices {
-//            val loggingInterceptor =
-//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-//            val client = OkHttpClient.Builder()
-//                .addInterceptor(loggingInterceptor)
-//                .build()
-//            val retrofit = Retrofit.Builder()
-//                .baseUrl("https://api.github.com/")
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .client(client)
-//                .build()
-//            return retrofit.create(ApiServices::class.java)
-//        }
-//    }
 
     private const val BASE_URL = "https://api.github.com/"
 
